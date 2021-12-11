1dc311ba73de9092ea2a3b019549e146969c842e
chezhia <chezhipower@gmail.com>
2021-12-11 18:48:46 -0500

    Switching to personal fork from main repo

--- course4/week2-ungraded-labs/C4_W2_Lab_2_Intro_to_Kubernetes/request.sh
+++ course4/week2-ungraded-labs/C4_W2_Lab_2_Intro_to_Kubernetes/request.sh
@@ -2,6 +2,7 @@
 
 while sleep 0.01;
 
-do curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST $(minikube ip):30001/v1/models/half_plus_two:predict;
+do curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST http://127.0.0.1:51604/v1/models/half_plus_two:predict;
+#do curl -d '{"instances": [1.0, 2.0, 5.0]}' -X POST $(minikube ip):30001/v1/models/half_plus_two:predict;
 
 done
